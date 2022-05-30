063856819ba16419b8778ea4a787bfb4729480eb
CUBRID <kdm97912@gmail.com>
2022-05-30 23:07:55 +0900

    2편 프로젝트 생성

---
+++ itemservice-db/src/main/java/hello/itemservice/service/ItemService.java
@@ -0,0 +1,19 @@
+package hello.itemservice.service;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.service.repository.ItemSearchCond;
+import hello.itemservice.service.repository.ItemUpdateDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService {
+
+    Item save(Item item);
+
+    void update(Long itemId, ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findItems(ItemSearchCond itemSearch);
+}
